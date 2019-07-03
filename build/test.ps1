17e28ec6ca610882b95b5f2b39a456d77f6bd09a
Luk Vermeulen <luk.vermeulen@gmail.com>
2019-07-03 13:25:05 +0200

    change build scripts & artifacts path

--- build/test.ps1
+++ build/test.ps1
@@ -1,5 +1,11 @@
-# run restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\..\test -Filter project.json -Recurse | ForEach-Object { & dotnet restore $_.FullName 2>&1 }
+foreach ($test in ls $PSScriptRoot\..\test/*) {
+    Push-Location $test
 
-# run tests
-Get-ChildItem -Path $PSScriptRoot\..\test -Filter project.json -Recurse | ForEach-Object { & dotnet test -c Release $_.FullName 2>&1 }
+	Write-Output "build: Testing project in $test"
+
+	& dotnet restore --no-cache
+    & dotnet test
+    if($LASTEXITCODE -ne 0) { exit 1 }    
+
+    Pop-Location
+}

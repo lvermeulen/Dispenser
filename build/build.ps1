a220916b3212f8ac19deaa1f487b53d606616aa9
Luk Vermeulen <luk.vermeulen@gmail.com>
2019-10-10 09:18:50 +0200

    use msbuild

--- build/build.ps1
+++ build/build.ps1
@@ -3,23 +3,23 @@ param (
 	[string]$TagVersionNumber
 )
 
-& dotnet restore --no-cache
+& msbuild /t:restore Dispenser.sln
 
-foreach ($src in ls $PSScriptRoot\..\src/*) {
-    Push-Location $src
-
-	Write-Output "build: Building & packaging project in $src"
-
-    if ($TagVersionNumber -ne $null) {
-        $version = $TagVersionNumber
-    }
-    else {
-        $version = $BuildVersionNumber
-    }
-
-    & dotnet build -c Release
-    & dotnet pack -c Release --include-symbols -o ..\..\artifacts --no-build /p:PackageVersion=$version
-    if($LASTEXITCODE -ne 0) { exit 1 }    
-
-    Pop-Location
-}
+#foreach ($src in ls $PSScriptRoot\..\src/*) {
+#    Push-Location $src
+#
+#	Write-Output "build: Building & packaging project in $src"
+#
+#    if ($TagVersionNumber -ne $null) {
+#        $version = $TagVersionNumber
+#    }
+#    else {
+#        $version = $BuildVersionNumber
+#    }
+#
+#    & dotnet build -c Release
+#    & dotnet pack -c Release --include-symbols -o ..\..\artifacts --no-build /p:PackageVersion=$version
+#    if($LASTEXITCODE -ne 0) { exit 1 }    
+#
+#    Pop-Location
+#}
